d1ec4dab4285d692e59aec4f5f508ef97a26ddd3
DL302 <32117423+DL302@users.noreply.github.com>
2020-06-05 14:12:04 -0700

    finished assignment 5

---
+++ 05_squares/squarefunc.h
@@ -0,0 +1,1 @@
+void squares(int size1, int x_offset, int y_offset, int size2);
---
+++ 05_squares/squares_test.c
@@ -0,0 +1,6 @@
+#include "squarefunc.h"
+#include <stdio.h>
+int main(int argc, char **argv)
+{
+    squares((*argv[1] - '0'), (*argv[2] - '0'), (*argv[3] - '0'), (*argv[4] - '0'));
+}
