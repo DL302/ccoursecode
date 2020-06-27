0c298851caa7dd39be3d92315bfb9840f145a579
DL302 <32117423+DL302@users.noreply.github.com>
2020-06-26 23:13:38 -0700

    changed test-power.c to fix error

--- 22_tests_power/test-power.c
+++ 22_tests_power/test-power.c
@@ -10,8 +10,8 @@ int main()
 {
     int size = 9;
     unsigned x[] = {0, 1, 2, 3, 4, 0, 10, 2, 10};
-    unsigned y[] = {0, 3, 2, 0, 2, 9, 10, 10, 2};
-    unsigned expected[] = {1, 1, 4, 1, 16, 0, 10000000000, 1024, 100};
+    unsigned y[] = {0, 3, 2, 0, 2, 9, 5, 10, 2};
+    unsigned expected[] = {1, 1, 4, 1, 16, 0, 100000, 1024, 100};
     unsigned testResult;
     for (int i = 0; i < size; i++)
     {
