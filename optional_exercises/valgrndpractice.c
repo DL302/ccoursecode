6871e0a4988b2c13c6287da37ed614fd0fad825b
DL302 <32117423+DL302@users.noreply.github.com>
2020-07-20 21:46:46 -0700

    finished valgrind optional assignment

--- optional_exercises/valgrndpractice.c
+++ optional_exercises/valgrndpractice.c
@@ -13,18 +13,21 @@ int main(void)
     size_t sz = 0;
     long *array = NULL;
     size_t n = 0;
-    while (getline(&line, &sz, stdin) > 0)
+    while (getline(&line, &sz, stdin) > 1)
     {
         n++;
         array = realloc(array, n * sizeof(*array));
-        array[n] = strtol(line, NULL, 0);
+        array[n - 1] = strtol(line, NULL, 0);
+        //program was originally going beyond bounds of array
     }
     free(line);
-    qsort(&array, n, sizeof(*array), cmplong);
+    qsort(array, n, sizeof(*array), cmplong);
     for (size_t i = 0; i < n; i++)
     {
         printf("%ld\n", array[i]);
-        free(&array[i]);
+        //program was originally `free`ing memory that wasn't malloc'd
     }
+    free(array);
+    //program was originally passing in a pointer to a non malloc'd memory
     return EXIT_SUCCESS;
 }
